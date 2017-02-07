3b62cc4dc8521030d05528afbbe276b04006592b
Kevin Orr <kevinorr54@gmail.com>
2017-02-06 21:35:22 -0500

    Added hacknight chals

---
+++ csaw_hacknight/100-guess_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import re, socket, time
+
+def test(i):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('hn.csaw.io', 9000))
+    s.recv(1024)
+
+    s.send(str(i).encode('ascii') + b'\n')
+    time.sleep(0.05)
+
+    return s.recv(1024)
+
+for i in range(1000):
+    resp = test(i)
+    print(resp.decode('ascii'), end='')
+
+    msg = resp.split(b'\r\n')[1]
+    if msg not in (b'kek'):
+        break
